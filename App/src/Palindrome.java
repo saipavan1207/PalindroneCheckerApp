d95a0c7ff9d2d9baeb639db4adffb408ecce3048
vivansanghvi <magicianvivan@gmail.com>
2026-03-03 14:18:45 +0530

    uc1 completed bt [pavan]

---
+++ App/src/Palindrome.java
@@ -0,0 +1,7 @@
+public class Palindrome {
+    static void main() {
+        System.out.println("welcome to palindrome checker");
+        System.out.println("version 1.0");
+        System.out.println("system intialised");
+    }
+}
