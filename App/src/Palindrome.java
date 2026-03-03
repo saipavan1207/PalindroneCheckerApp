107556731820cba03a1f140876727d4cf8f47bb4
vivansanghvi <magicianvivan@gmail.com>
2026-03-03 14:51:49 +0530

    uc2 completed bt [pavan]

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -3,5 +3,20 @@ public class Palindrome {
         System.out.println("welcome to palindrome checker");
         System.out.println("version 1.0");
         System.out.println("system intialised");
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " not a palindrome.");
+        }
     }
 }
